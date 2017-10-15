68fa109fa372395525e7dd37c9c98276ea8996c5
yrrag5 <G00335806@gmit.ie>
2017-10-15 19:01:03 +0100

    Linking local repo

---
+++ src/Q1.go
@@ -0,0 +1,14 @@
+
+// Author: Garry Cummins
+// Date: 23-09-2017
+
+// Adapted from : https://tour.golang.org/welcome/1
+
+package main
+
+import "fmt"
+
+func main() {
+	// Displays the message Hello World! in Japanese 
+	fmt.Println("こんにちは世界!") 
+}
