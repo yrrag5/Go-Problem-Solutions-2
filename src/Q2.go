b05f8ece1e7d6a99d57add360a4fa4ea41fd6ad6
yrrag5 <G00335806@gmit.ie>
2017-10-23 13:28:51 +0100

    Adding Q2 solution with comments- h1 tags

---
+++ src/Q2.go
@@ -0,0 +1,22 @@
+// Author: Garry Cummins
+// Date: 23/10/2017
+
+
+package main
+
+import (
+  "net/http"
+)
+
+func main() {
+  http.HandleFunc("/", guess)
+  http.ListenAndServe(":8080", nil)
+}
+
+func guess(w http.ResponseWriter, r *http.Request) {
+	//Adding header tags to guessing game
+	w.Header().Set("Server", "Guessing game")
+  	w.WriteHeader(200)
+	//Adding header tags to guessing game
+  	fmt.Fprint(w, "<h1>Guessing Game</h1>")
+}
